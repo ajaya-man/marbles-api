70d6dd020f0bf86845364ebafad4f78975f8e39b
Ajaya Mandal <ajaymandal1996@gmail.com>
2019-05-02 14:03:43 +0545

    Update invoke.js

--- api/middleware/invoke.js
+++ api/middleware/invoke.js
@@ -1,12 +1,4 @@
 'use strict';
-/*
-* Copyright IBM Corp All Rights Reserved
-*
-* SPDX-License-Identifier: Apache-2.0
-*/
-/*
- * Chaincode Invoke
- */
 
 var Fabric_Client = require('fabric-client');
 var path = require('path');
